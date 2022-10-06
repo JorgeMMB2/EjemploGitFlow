ea0a11954378e154696147a31819ece8ba8c9859
Jorge Martín <jormartindos@gmail.com>
2022-10-06 20:25:00 +0200

    Perros añadidos

---
+++ src/com/ufv/Perritos.java
@@ -0,0 +1,14 @@
+package com.ufv;
+
+public class Perritos {
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    private String raza;
+
+}
