136f13b879282ec881699d0900021c29cff06ebe
Jorge Martín <jormartindos@gmail.com>
2022-10-06 19:53:27 +0200

    CAMBIOS

--- src/com/ufv/Main.java
+++ src/com/ufv/Main.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Persona per = new Persona();
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("Escribe tu nombre: ");
+
         String cadena = sc.nextLine();
-        System.out.println("Tu nombre es ----> "+ cadena);
 
         System.out.println("Escribe tu nombre: ");
         per.setNombre(sc.nextLine());
         System.out.println("Escribe tu apellido: ");
         per.setApellidos(sc.nextLine());
+
+
+
+
+        System.out.println("Tu nombre es ----> "+ cadena);
+
 
     }
 }
